6b7c9271f8f28dbd7d5b1650ca87b898715eadd3
shreyakarthik1210 <52420053+shreyakarthik1210@users.noreply.github.com>
2023-12-27 21:20:56 -0600

    Update duck.sh

--- duck.sh
+++ duck.sh
@@ -1,1 +1,1 @@
-echo url="https://www.duckdns.org/update?domains=birdieut.duckdns.org&token=36c498d4-bbb3-4c99-ae59-4d12456e0d21&ip=" | curl -k -o ~/duckdns/duck.log -K -
+echo url=TOKEN | curl -k -o ~/duckdns/duck.log -K -
